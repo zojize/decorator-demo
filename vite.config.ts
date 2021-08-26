46d41545ee7c5d382e2670c9a4b133e8ee2fefc0
SIGUSR97 <jeffzo01@qq.com>
2021-08-26 22:56:02 +0800

    fix: build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
-import { defineConfig } from "vite";
-import typescript from "@rollup/plugin-typescript";
-import swc from "rollup-plugin-swc";
+import { defineConfig } from 'vite';
+import typescript from '@rollup/plugin-typescript';
+import swc from 'rollup-plugin-swc';
 
 // import typescript from "rollup-plugin-typescript2";
 
+const swcPlugin = (() => {
+  const plugin = swc({
+    test: 'ts',
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        dynamicImport: true,
+        decorators: true,
+      },
+      target: 'es2021',
+      transform: {
+        decoratorMetadata: true,
+      },
+    },
+  });
+
+  const originalTransform = plugin.transform!;
+
+  const transform = function (...args: Parameters<typeof originalTransform>) {
+    if (!args[1].endsWith('html')) return originalTransform.apply(this, args);
+  };
+
+  return { ...plugin, transform };
+})();
+
 export default defineConfig({
-    plugins: [
-        swc({
-            jsc: {
-                parser: {
-                    syntax: "typescript",
-                    dynamicImport: true,
-                    decorators: true,
-                },
-                target: "es2021",
-                transform: {
-                    decoratorMetadata: true,
-                },
-            },
-        }),
-    ],
-    esbuild: false,
+  plugins: [swcPlugin],
+  // esbuild: false,
+  build: {},
 });
